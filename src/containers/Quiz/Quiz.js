b2b185147515ffa1a5ff2ffd31a0cd727312c065
Volodymyr Kolchin <vkolchin@codifi.me>
2021-11-24 15:53:28 +0200

    Added func onAnswerClickHandler

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -3,21 +3,39 @@ import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
-  state = {
-    quiz: []
-  }
+    state = {
+        quiz: [
+            {
+                question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                answers: [
+                    {text: 'Черный', id: 1},
+                    {text: 'Синий', id: 2},
+                    {text: 'Красный', id: 3},
+                    {text: 'Зеленый', id: 4}
+                ]
+            }
+        ]
+    }
 
-  render() {
-    return (
-      <div className={classes.Quiz}>
-        <div className={classes.QuizWrapper}>
-            <h1>Quiz</h1>
+    onAnswerClickHandler = answerId => {
+        console.log('Answer Id: ', answerId)
+    }
+    render() {
+        return (
+          <div className={classes.Quiz}>
+            <div className={classes.QuizWrapper}>
+              <h1>Ответьте на все вопросы</h1>
 
-            <ActiveQuiz />
-        </div>
-      </div>
-    )
-  }
+              <ActiveQuiz
+                answers={this.state.quiz[0].answers}
+                question={this.state.quiz[0].question}
+                 onAnswerClick={this.onAnswerClickHandler}
+              />
+            </div>
+          </div>
+        )
+    }
 }
 
 
